58c27f69bc3e13d23db34a0d86019585dd7ba2b6
Gabriel Alejandro Pina <gabrielpinia@hotmail.com>
2023-11-02 09:01:18 -0400

    1er Modulo Alumnos

---
+++ practicas-backend/src/main/java/com/microservice/usuarios/PracticasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasBackendApplication.class, args);
+	}
+
+}
